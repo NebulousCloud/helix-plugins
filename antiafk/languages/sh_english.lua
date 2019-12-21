b9fab14a36509643c27c877f99e5fc7ff782f0ea
Igor Radovanovic <impulsh@users.noreply.github.com>
2019-12-21 00:42:52 -0500

    Add plugin center

--- antiafk/languages/sh_english.lua
+++
@@ -1,6 +0,0 @@
-LANGUAGE = {
-	antiafk = "Anti-AFK",
-	kickedAFK = "%s has been auto-kicked by the server (AFK)",
-	kickedAdminAFK = "%s has auto-kicked '%s' (AFK).",
-	charAFK = "Away from keyboard"
-}
--- flashlightitem/README.md
+++
@@ -1,6 +0,0 @@
-# ix-flashlight
-A flashlight item for Helix. Released to the public domain.
-
-## Installation
-- Download the zip
-- Extract it into your Schema's plugin folder
