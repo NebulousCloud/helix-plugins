45420996cfb2b24588bce90665bece399f2e1548
Gary Tate <me@garytate.co.uk>
2019-12-22 20:17:41 +0000

    Accidentally included obfuscated log command

--- bodygroupmanager/sv_hooks.lua
+++ bodygroupmanager/sv_hooks.lua
@@ -1,7 +1,6 @@
 local PLUGIN = PLUGIN
 
 netstream.Hook("ixBodygroupTableSet", function(client, target, bodygroups)
-    ix.log.Add(client, "editbodygroup", target)
     for k, v in pairs(bodygroups) do
         target.player:SetBodygroup(k, v)
     end
