d4cc3da04ba8b7ef75574c7426688b5b2832775e
Igor Radovanovic <impulsh@users.noreply.github.com>
2019-12-21 20:07:06 -0500

    Fix missing license for bettedesc.lua, weight
    Typo in the spelling for license

--- betterdesc.lua
+++ betterdesc.lua
@@ -2,7 +2,7 @@ PLUGIN.name = "Better Description Display"
 PLUGIN.author = "Vex"
 PLUGIN.description = "A better description display for when you look at people."
 
-PLUGIN.licence = [[
+PLUGIN.license = [[
 Copyright 2019 Chris Russell
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, and/or distribute copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
