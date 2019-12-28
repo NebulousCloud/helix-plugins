8bb2c8eaae4564f79ba3e309546efa450e414336
Igor Radovanovic <impulsh@users.noreply.github.com>
2019-12-27 19:59:50 -0500

    Fix license for Anti-AFK plugin
    Closes #15

--- antiafk/sh_plugin.lua
+++ antiafk/sh_plugin.lua
@@ -1,12 +1,12 @@
---[[
-(c) 2016 by Gr4Ss (greengr4ss@gmail.com)
-This plugin is licensed under the Creative Commons Attribution-ShareAlike 4.0 International License.
-To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/
-]]
 
 PLUGIN.name = "Anti-AFK"
 PLUGIN.author = "Gr4Ss"
 PLUGIN.description = "Stops AFK players from earning wages and kicks when server is full."
+PLUGIN.license = [[
+(c) 2016 by Gr4Ss (greengr4ss@gmail.com)
+This plugin is licensed under the Creative Commons Attribution-ShareAlike 4.0 International License.
+To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/
+]]
 
 ix.util.Include("cl_hooks.lua")
 ix.util.Include("sh_configs.lua")
