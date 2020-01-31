4e55c0c0085e87730b2fdcc3c1d623f69ea10321
wowmod <mrsonyplays@gmail.com>
2020-01-31 01:34:56 -0600

    Inital Commit of Gr4ss Waypoints

---
+++ waypoints/languages/sh_english.lua
@@ -0,0 +1,26 @@
+
+LANGUAGE = {
+	cmdWaypointAdd = "Add a waypoint where you are looking.",
+	cmdWaypointAddND = "Add a waypoint with no distance marking where you are looking.",
+	cmdWaypointUpdate = "Update an existing waypoint closest to where you are looking.",
+
+	cmdWaypointRemove = "Remove an existing waypoint closest to where you are looking.",
+	cmdWaypointRemoveAll = "Remove all current waypoints.",
+
+	addedWaypoint = "You have added a waypoint.",
+	updatedWaypoint = "You have updated a waypoint.",
+
+	removedWaypoint = "You have removed a waypoint.",
+	removedAllWaypoints = "You have removed all waypoints.",
+
+	cannotAddWaypoints = "You cannot add waypoints.",
+	cannotRemoveWaypoints = "You cannot remove waypoints.",
+	invalidWaypointColor = "'%s' is not a valid color! Use one of these instead:\n%s",
+
+	noWaypointsToRemove = "There are no waypoints that you can remove.",
+	noWaypointsNearToRemove = "There are no waypoints that you can remove near where you are looking.",
+	noWaypointsToUpdate = "There are no waypoints you can update.",
+	noWaypointsNearToUpdate = "There are no waypoints you can update near where you are looking.",
+
+	noWaypointsSet = "There are no waypoints set."
+}
---
+++ waypoints/libs/sv_waypoints.lua
@@ -0,0 +1,17 @@
+
+local PLUGIN = PLUGIN
+
+local curTime = CurTime()
+
+Schema.waypoints = Schema.waypoints or {}
+
+function Schema.waypoints:AddWaypoint(position, text, color, time, addedBy)
+	local waypoint = {}
+	waypoint.pos = position + Vector(0, 0, 30)
+	waypoint.text = text
+	waypoint.color = color
+	waypoint.addedBy = addedBy
+	waypoint.time = CurTime() + time
+
+	PLUGIN:AddWaypoint(waypoint)
+end
