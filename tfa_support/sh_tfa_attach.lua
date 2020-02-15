b39748e83d751a3c521aa0e69bc132cda15b7761
Taxin2012 <mrtaxin2012@mail.ru>
2020-02-15 17:41:41 +0300

    Create sh_tfa_attach.lua

---
+++ tfa_support/sh_tfa_attach.lua
@@ -0,0 +1,9 @@
+PLUGIN.AttachData[ "ins2_si_kobra" ] = {
+	Name = "Kobra Sight",
+	Desc = "Sight for weapons",
+	Price = 2000,
+	Model = "models/Items/BoxSRounds.mdl",
+	Width = 1,
+	Height = 1,
+	Slot = 2
+}
