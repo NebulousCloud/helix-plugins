af10841c3489b54596c8adc779c6ebc107abfe19
Taxin2012 <mrtaxin2012@mail.ru>
2020-02-15 17:42:27 +0300

    Create sh_tfa_weps.lua

---
+++ tfa_support/sh_tfa_weps.lua
@@ -0,0 +1,22 @@
+PLUGIN.GunData[ "tfa_ins2_ak74m" ] = {
+	Slot = "primary",
+	Width = 4,
+	Height = 2,
+	Weight = 3,
+	Price = 2000,
+	
+	--Weapon Parameters
+	--Prim == Primary
+	Prim = {
+		Ammo = "assault",
+		Damage = 31,
+		KickUp = 0.4,
+		KickDown = 0.4,
+		KickHorizontal = 0.35,
+		Spread = .021,
+		IronAccuracy = .01
+	}
+	
+	--Sec == Secondary
+	Sec = {}
+}
