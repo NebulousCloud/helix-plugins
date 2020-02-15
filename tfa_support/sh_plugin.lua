a9d1fa9cfe472b9e10c5b9f0f141f5921aa2dea4
Taxin2012 <mrtaxin2012@mail.ru>
2020-02-15 17:36:42 +0300

    Create sh_plugin.lua

---
+++ tfa_support/sh_plugin.lua
@@ -0,0 +1,19 @@
+local PLUGIN = PLUGIN
+PLUGIN.name = "TFA Support"
+PLUGIN.author = "Taxin2012"
+PLUGIN.description = "Support for TFA Weapons Pack"
+PLUGIN.license = [[Copyright 2019 Taxin2012
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.]]
+
+
+
+ix.util.Include("sh_tfa_support.lua")
+ix.util.Include("cl_tfa_support.lua")
