98911ffe4d429d824994a1f1cb94d579d87b5014
Gary Tate <me@garytate.co.uk>
2020-02-23 23:26:22 +0000

    Registers privilege before command usage.

--- itemspawner/sh_plugin.lua
+++ itemspawner/sh_plugin.lua
@@ -4,6 +4,11 @@ PLUGIN.name = "Item Spawner System"
 PLUGIN.author = "Gary Tate"
 PLUGIN.description = "Allows staff to select item spawn points with great configuration."
 
+CAMI.RegisterPrivilege({
+	Name = "Helix - Item Spawner",
+	MinAccess = "admin"
+})
+
 ix.util.Include("sh_config.lua")
 ix.util.Include("sh_commands.lua")
 ix.util.Include("sv_hooks.lua")
