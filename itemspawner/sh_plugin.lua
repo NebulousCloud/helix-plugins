f111e346d7fd46e60148cdbda1ae04c5fd5a5b9b
Gary Tate <me@garytate.co.uk>
2020-02-23 23:19:40 +0000

    Fixed bad argument to next

--- itemspawner/sh_plugin.lua
+++ itemspawner/sh_plugin.lua
@@ -4,18 +4,6 @@ PLUGIN.name = "Item Spawner System"
 PLUGIN.author = "Gary Tate"
 PLUGIN.description = "Allows staff to select item spawn points with great configuration."
 
-PLUGIN.spawner = PLUGIN.spawner or {}
-PLUGIN.items = PLUGIN.items or {}
-PLUGIN.spawner.positions = PLUGIN.spawner.positions or {}
-
-PLUGIN.items.common = {
-	"pistol"
-}
-
-PLUGIN.items.rare = {
-	"shotgun"
-}
-
 ix.util.Include("sh_config.lua")
 ix.util.Include("sh_commands.lua")
 ix.util.Include("sv_hooks.lua")
