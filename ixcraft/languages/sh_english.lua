f1868fa5b39a9bb260e871ee1cad66d352e84636
zgredinzyyy <zgredinzyyy@gmail.com>
2020-05-02 15:44:16 +0200

    Small language fix

--- ixcraft/languages/sh_english.lua
+++ ixcraft/languages/sh_english.lua
@@ -1,9 +1,9 @@
 
 LANGUAGE = {
-	["CraftMissingFlag"] = "You are missing the %s flag.",
-	["CraftMissingTool"] = "You are missing a %s.",
-	["CraftMissingItem"] = "You are missing: %s.",
-	["CraftSuccess"] = "You have successfully crafted %s.",
+	CraftMissingFlag = "You are missing the %s flag.",
+	CraftMissingTool = "You are missing a %s.",
+	CraftMissingItem = "You are missing: %s.",
+	CraftSuccess = "You have successfully crafted %s.",
 
-	["cmdCraftRecipe"] = "Attempt to craft a recipe."
+	cmdCraftRecipe = "Attempt to craft a recipe."
 }
