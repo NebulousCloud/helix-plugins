37ceb0c3aafd149be148691f78d326415eca2d78
zgredinzyyy <zgredinzyyy@gmail.com>
2020-05-02 15:45:20 +0200

    Create sh_polish.lua

---
+++ ixcraft/languages/sh_polish.lua
@@ -0,0 +1,9 @@
+
+LANGUAGE = {
+	CraftMissingFlag = "Nie masz flagi %s.",
+	CraftMissingTool = "Nie posiadasz %s.",
+	CraftMissingItem = "Brakuje Ci: %s.",
+	CraftSuccess = "Poprawnie wytworzyłeś %s.",
+
+	cmdCraftRecipe = "Spróbuj stworzyć przepis."
+}
