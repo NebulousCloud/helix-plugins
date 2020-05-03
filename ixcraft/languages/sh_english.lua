ae07589832c22b8df6a9219528b6f16730c29ac5
wowm0d <mrsonyplays@gmail.com>
2020-05-03 14:14:41 -0500

    Update Better Crafting plugin (#59)

--- ixcraft/languages/sh_english.lua
+++ ixcraft/languages/sh_english.lua
@@ -5,5 +5,9 @@ LANGUAGE = {
 	["CraftMissingItem"] = "You are missing: %s.",
 	["CraftSuccess"] = "You have successfully crafted %s.",
 
+	["CraftTools"] = "TOOLS",
+	["CraftRequirements"] = "REQUIREMENTS",
+	["CraftResults"] = "RESULTS",
+
 	["cmdCraftRecipe"] = "Attempt to craft a recipe."
 }
