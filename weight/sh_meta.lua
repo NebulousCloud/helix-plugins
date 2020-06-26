877beb05131fbdea6f6abbeadd6416f8a653375f
DefenderVex <DefenderVex@users.noreply.github.com>
2020-06-26 16:08:56 +1200

    Updated weight plugin (#72)

--- weight/sh_meta.lua
+++ weight/sh_meta.lua
@@ -5,6 +5,16 @@ function ITEM:GetWeight()
 	return self:GetData("weight", nil) or self.weight or nil
 end
 
+function ITEM:DropWeight(w)
+	if (self.player) then
+		local weight = w or self:GetWeight()
+
+		if (weight) then
+			self.player:GetCharacter():DropWeight(weight)
+		end
+	end
+end
+
 ix.meta.item = ITEM
 -- ITEM META END --
 
@@ -12,13 +22,18 @@ ix.meta.item = ITEM
 local CHAR = ix.meta.character or {}
 
 function CHAR:Overweight()
-	return self:GetData("carry", 0) > ix.config.Get("maxWeight", 30)
+	return self:GetData("carry", 0) > ix.weight.BaseWeight(self)
 end
 
 function CHAR:CanCarry(item)
-	return ix.weight.CanCarry(item:GetWeight(), self:GetData("carry", 0))
+	return ix.weight.CanCarry(item:GetWeight(), self:GetData("carry", 0), self)
 end
 
+function CHAR:UpdateWeight()
+	ix.weight.Update(self)
+end
+
+-- these are primarily intended as internally used functions, you shouldn't use them in your own code --
 function CHAR:AddCarry(item)
 	self:SetData("carry", math.max(self:GetData("carry", 0) + item:GetWeight(), 0))
 end
@@ -26,6 +41,7 @@ end
 function CHAR:RemoveCarry(item)
 	self:SetData("carry", math.max(self:GetData("carry", 0) - item:GetWeight(), 0))
 end
+-- these are primarily intended as internally used functions, you shouldn't use them in your own code --
 
 function CHAR:DropWeight(weight)
 	self:SetData("carry", math.max(self:GetData("carry", 0) - weight, 0))
