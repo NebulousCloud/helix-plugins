1f9286ed3ca3a8bc80665bca66f258d807652bda
Aspect <41125780+Aspect12@users.noreply.github.com>
2023-04-23 21:21:00 +0300

    Add Live Dr. Breen Reaction plugin. (#114)

---
+++ live_breen_reaction/sh_plugin.lua
@@ -0,0 +1,6 @@
+
+PLUGIN.name = "Live Breen Reaction"
+PLUGIN.author = "Aspect™"
+PLUGIN.description = "A joke plugin that adds a socio-status-relevant Dr. Breen reaction to the upper-right corner of the screen."
+
+ix.util.Include("cl_hooks.lua")
