84138f303f20fd451b07aada4bbeeece59c03514
Andrés <57843384+AndresAmaro@users.noreply.github.com>
2025-01-13 22:17:41 +0000

    Simple Bodygroup Manager pluign (#116)

---
+++ simple_bodygroup_manager/languages/sh_spanish.lua
@@ -0,0 +1,15 @@
+LANGUAGE = {
+    cfgColorFondoBGManager = "Color del fondo",
+    cfgColorEnfasisBGManager = "Color del enfasis",
+    cfgAllowKeyUseBGManager = "Permitir uso con tecla",
+    cfgSaveButtonColor = "Color del botón de guardado",
+
+    cmdBanKeyUseSBMDesc = "Impide que un jugador pueda utilizar el menú con la tecla aunque esté activado",
+    cmdEditUserBodygroups = "Modifica los bodygroups de un usuario desde el menú.",
+    cmdBanKeyUseSBMUnbanned = "El staff %s ha retirado la restricción del uso de tecla a %s.",
+    cmdBanKeyUseSBMBanned = "El staff %s ha restringido a %s de abrir el gestor de bodygroups con la tecla de acceso rápido.",
+
+    selfBodygroupsModified = "¡Has guardado tu apariencia!",
+    userBodygroupsModified = "Has modificado la apariencia de %s.",
+
+}
