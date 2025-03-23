67e590c21061ac655f14a872284a7ec31518170c
Bena <57843384+SoyBena@users.noreply.github.com>
2025-03-23 22:52:41 +0000

    Now the skin is properly changed

--- simple_bodygroup_manager/net/sv_net.lua
+++ simple_bodygroup_manager/net/sv_net.lua
@@ -5,6 +5,8 @@ util.AddNetworkString("SBMSaveBodygroups")
 net.Receive("SBMSaveBodygroups", function(_, ply)
     local target = net.ReadPlayer()
     local tblBodygroups = net.ReadTable()
+    local skin = net.ReadUInt(5)
+
 
     if (!target or !target:IsPlayer() or !target:GetCharacter()) then return end
 
@@ -12,6 +14,7 @@ net.Receive("SBMSaveBodygroups", function(_, ply)
         target:SetBodygroup(id, val)
     end
 
+    target:SetSkin(skin)
     target:GetCharacter():SetData("groups", tblBodygroups)
 
 end)
